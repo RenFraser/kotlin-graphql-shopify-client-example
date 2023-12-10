a64f08002630a0a523da760bd25ee77eb4238ca7
Henry Fraser <henrymfraser@gmail.com>
2023-12-10 11:45:51 +1000

    Add client library usage example

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import com.expediagroup.graphql.plugin.gradle.config.GraphQLScalar
+import com.expediagroup.graphql.plugin.gradle.config.GraphQLSerializer
+import com.expediagroup.graphql.plugin.gradle.graphql
+
+
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("com.expediagroup.graphql") version "7.0.2"
 }
 
 group = "org.example"
@@ -10,12 +16,34 @@ repositories {
 }
 
 dependencies {
+    implementation("com.expediagroup:graphql-kotlin-spring-client:7.0.2")
+    implementation("com.expediagroup:graphql-kotlin-schema-generator:7.0.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+
 kotlin {
     jvmToolchain(17)
 }
+
+graphql {
+    client {
+        packageName = "org.example"
+        endpoint = "https://your-myshopify-domain-here/admin/api/unstable/graphql.json"
+        allowDeprecatedFields = true
+        headers = mapOf("X-Shopify-Access-Token" to "your-shopify-access-token-here")
+        customScalars = listOf(
+            GraphQLScalar("Locale", "com.ibm.icu.util.ULocale", "com.expediagroup.graphql.examples.client.gradle.ULocaleScalarConverter"),
+            GraphQLScalar("UUID", "java.util.UUID", "com.expediagroup.graphql.examples.client.gradle.UUIDScalarConverter"),
+        )
+        serializer = GraphQLSerializer.JACKSON
+
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,36 @@
 package org.example
 
+import com.expediagroup.graphql.client.spring.GraphQLWebClient
+import kotlinx.coroutines.runBlocking
+import org.springframework.web.reactive.function.client.WebClient
+
 fun main() {
-    println("Hello World!")
+
+    // You can also pass in an existing web client.
+    // See: https://opensource.expediagroup.com/graphql-kotlin/docs/client/client-customization/#global-client-customization-1
+
+    val webClientBuilder = WebClient
+        .builder()
+        .defaultHeader("X-Shopify-Access-Token", "your-access-token-here")
+
+    val client = GraphQLWebClient(
+        url = "https://your-myshopify-domain-here/admin/api/unstable/graphql.json",
+        builder = webClientBuilder
+    )
+
+    runBlocking {
+        val shopQuery = ShopQuery()
+        val result = client.execute(shopQuery)
+
+        val errors = result.errors ?: listOf()
+
+        if (errors.isNotEmpty()) {
+            println(errors)
+        }
+
+        val shop = checkNotNull(result.data?.shop)
+
+        println(shop.id)
+        println(shop.name)
+    }
 }
